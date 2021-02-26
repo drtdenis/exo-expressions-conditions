5dbf1ebcf5139122883c24ade50c403252d1e950
drtdenis <drouetdenis2@gmail.com>
2021-02-26 10:25:18 +0100

    welcom.js

--- welcome.js
+++ welcome.js
@@ -1,2 +1,8 @@
-let firstname = " BOB"
-let gender = "male"
+/*let firstname = " BOB"
+let gender = "male"*/
+
+// corrections du 26/02/2021
+let firstname = "Alice"
+let gender = "female"
+//let msg = Bob ? 'bonjour Bob' : 'bonjour Alice'
+console.log(`Bonjour ${firstname}, Vous etes entré${gender === "female" ? "e" : ""} sur le chan`)
