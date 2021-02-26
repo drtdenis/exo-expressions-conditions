371149734954f78f524c9676f8c15bd26f398582
drtdenis <drouetdenis2@gmail.com>
2021-02-26 01:27:46 +0100

    fixMe.js

--- fixMe.js
+++ fixMe.js
@@ -1,20 +1,23 @@
-/*let list = "'Alice', 'Bob', 'Charlie', 'Craig', 'Eva'"
+/*let list = ['Alice', 'Bob', 'Charlie', 'Craig', 'Eva']
 for (let j = 0; j <= list.length; ++i) {
-  list[i] = list[i].toLowerCase()
+  list[i] = list[i].toUpperCase()
   switch (list[i]) {
-    Case ALICE:
-  consol.log('Alice want to exchange with Bob')
-  Case BOB:
-  console.log('Bob want to exchange with Alice')
-  Case CHARLIE:
-  console.log('Charlie is a generic third participant')
-  Case CRAIG:
-  console.log('Craig is a password cracker')
-  Case EVE:
-  console.log('Eve is an eavesdropper')
-}
+    case "ALICE":
+      consol.log('Alice want to exchange with Bob')
+      break;
+      Case"Bob":
+      console.log('Bob want to exchange with Alice')
+      break;
+      Case CHARLIE:
+      console.log('Charlie is a generic third participant')
+      break;
+      Case CRAIG:
+      console.log('Craig is a password cracker')
+      break;
+      Case EVE:
+      console.log('Eve is an eavesdropper')
+  }
 }*/
-
 //Vous devez corriger le code précédent et obtenir l'affichage suivant:
 
 //Alice want to exchange with Bob
@@ -22,3 +25,26 @@ for (let j = 0; j <= list.length; ++i) {
 //Charlie is a generic third participant
 //Craig is a password cracker
 //Eve is an eavesdropper
+
+
+let list = ['Alice', 'Bob', 'Charlie', 'Craig', 'Eva']
+for (let j = 0; j <= list.length; ++j) {
+  //list[j] = list[j].ToUpperCase()
+  switch (list[j]) {
+    case "Alice":
+      console.log('Alice want to exchange with Bob');
+      break;
+    case "Bob":
+      console.log('Bob want to exchange with Alice');
+      break;
+    case "Charlie":
+      console.log('Charlie is a generic third participant');
+      break;
+    case "CRAIG":
+      console.log('Craig is a password cracker');
+      break;
+    case "Eva":
+      console.log('Eva is an eavesdropper');
+      break;
+  }
+}
